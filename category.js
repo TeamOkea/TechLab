ee70c6d30d0a92c858d8eddc392fb3be20ff557d
Oliver <olla0002@stud.kea.dk>
2024-09-19 09:08:12 +0200

    last commit fra oli maybe

--- category.js
+++ category.js
@@ -1,35 +1,20 @@
-const categoryList = document.querySelector("#categoryList");
-const params = new URLSearchParams(document.location.search);
-const category = params.get("category");
-let url = undefined;
+fetch("https://njujbooqnwrtexzmzpjj.supabase.co/rest/v1/kea_website")
+  .then((res) => res.json())
+  .then(showCategories);
 
-fetch("https://kea-alt-del.dk/t7/api/categories")
-  .then((response) => response.json())
-  .then((categories) => {
-    categories.forEach((category) => {
-      categoryList.innerHTML += `<li><a href="produktliste.html?category=${category.category}">${category.category}</a></li>`;
-    });
-  });
-
-if (params.has("category")) {
-  url = `https://kea-alt-del.dk/t7/api/products?category=${category}`;
-} else {
-  url = "https://kea-alt-del.dk/t7/api/products";
+function showCategories(cats) {
+  cats.forEach(showCategory);
 }
 
-function duplicateTemplate(template, container) {
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-      data.forEach((product) => {
-        const templateClone = template.cloneNode(true);
-
-        templateClone.querySelector(".category").textContent = product.category;
-
-        container.appendChild(templateClone);
-      });
-    })
-    .catch((error) => console.log(error));
+function showCategory(cat) {
+  // fanger vores template
+  const template = document.querySelector("template").content;
+  // cloner
+  const clone = template.cloneNode(true);
+  // ændrer indhold
+  clone.querySelector("a").textContent = cat.category;
+  clone.querySelector("a").href = sub-category.html?category=${cat.category};
+  
+  // appender
+  document.querySelector(".content").appendChild(clone);
 }
-
-duplicateTemplate(productTemplate, productContainer);

537ef098c1d6bf4deca4cb7c2eec27a99f46f995
Oliver Valter <olva0001@kea.stud.dk>
2024-09-19 10:51:19 +0200

    Logo og det

--- productlist.js
+++ productlist.js
@@ -1,53 +1,60 @@
 /** @format */
-
+ 
 // https://njujbooqnwrtexzmzpjj.supabase.co
 // eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5qdWpib29xbndydGV4em16cGpqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU5NTU2MDAsImV4cCI6MjA0MTUzMTYwMH0.BfUJKtZC_blOYEL_J2_BaLEyjijCs9j4fqojU4kIfrg
-
+ 
 const params = new URLSearchParams(window.location.search);
-let selectedCategory = params.get("category");
-
-selectedCategory = "Mobile Devices";
-
-fetch(
-  `https://njujbooqnwrtexzmzpjj.supabase.co/rest/v1/TSL?Taksonomi=eq.${encodeURIComponent(
-    selectedCategory
-  )}`,
-  /* fetch(
+let category = params.get("category");
+let url =
+  "https://njujbooqnwrtexzmzpjj.supabase.co/rest/v1/kea_website?Type=" +
+  category;
+ 
+category = "Mærke";
+ 
+if (category)
+  fetch(
+    `https://njujbooqnwrtexzmzpjj.supabase.co/rest/v1/kea_website?.${encodeURIComponent(
+      category
+    )}`,
+ 
+    /* fetch(
   "https://njujbooqnwrtexzmzpjj.supabase.co/rest/v1/kea_website?Mærke=eq.GoPro?select=Objektkode", */
-  /*  fetch(
+    /*  fetch(
     "https://njujbooqnwrtexzmzpjj.supabase.co/rest/v1/kea_website?select=tags", */
-  {
-    method: "GET",
-    headers: {
-      apikey:
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5qdWpib29xbndydGV4em16cGpqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU5NTU2MDAsImV4cCI6MjA0MTUzMTYwMH0.BfUJKtZC_blOYEL_J2_BaLEyjijCs9j4fqojU4kIfrg",
-    },
-  }
-)
-  .then((response) => response.json())
-  .then(showDatas);
-
+    {
+      method: "GET",
+      headers: {
+        apikey:
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5qdWpib29xbndydGV4em16cGpqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU5NTU2MDAsImV4cCI6MjA0MTUzMTYwMH0.BfUJKtZC_blOYEL_J2_BaLEyjijCs9j4fqojU4kIfrg",
+      },
+    }
+  )
+    .then((response) => response.json())
+    .then(showDatas);
+ 
 function showDatas(items) {
   console.log(items);
   items.forEach(showData);
 }
-
-function showDatas(item) {
+ 
+function showData(item) {
   console.log(item);
-
+ 
   // fang template
   const template = document.querySelector("#smallProductTemplate");
-
+ 
   // lav en kopi
   const copy = template.content.cloneNode(true);
-
+ 
   //ændre indhold
-  copy.querySelector(".icon").textContent.innerHTML = item.Mærke;
-
+  copy.querySelector(".icon").textContent.innerHTML = item.Type;
+  copy.querySelector("a").textContent = item.Mærke;
+  copy.querySelector(".content-2").textContent = item.Type;
+  copy.querySelector(".content-3").textContent = item.Produktnavnogmodel;
   //appende
   document.querySelector(".container").appendChild(copy);
 }
-
+ 
 /* {
     "Type": " Camera Mounts",
     "Asset ID": "C00332",
